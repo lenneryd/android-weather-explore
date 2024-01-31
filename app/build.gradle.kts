dba31842d10a50db524dc7b31965e4db924e5953
Tim Lenneryd <tim.lenneryd@cygni.se>
2024-01-31 11:32:22 +0100

    Migrate to BOM.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,13 +50,14 @@ android {
 
 dependencies {
 
+    implementation(platform("androidx.compose:compose-bom:2024.01.00"))
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("androidx.compose.material:material-icons-core:1.5.4")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
@@ -75,7 +76,6 @@ dependencies {
     // Java language implementation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
-    implementation("androidx.compose.foundation:foundation-layout:1.5.4")
 
     // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
@@ -92,23 +92,20 @@ dependencies {
 
     // Jetpack Compose Integration
     implementation("androidx.navigation:navigation-compose:2.7.6")
-
     // Integration with activities
     implementation("androidx.activity:activity-compose:1.8.2")
-    // Compose Material Design
-    implementation("androidx.compose.material:material:1.5.4")
-    // Compose Material Design 3
+
+    implementation("androidx.compose.material:material")
     implementation("androidx.compose.material3:material3:1.1.2")
-    // Animations
-    implementation("androidx.compose.animation:animation:1.5.4")
-    // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.5.4")
-    // Integration with ViewModels
+    implementation("androidx.compose.foundation:foundation-layout")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.ui:ui-tooling")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     // Constraint Layout
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     // UI Tests
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
